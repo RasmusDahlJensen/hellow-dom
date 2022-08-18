1b42e2577db5c527a93bf274760bbaeb96001bf5
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-18 09:23:03 +0200

    Hello World aflevering

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+//Get elements
+const myApp = document.getElementById('app');
+
+
+//Create elements
+const newH1 = document.createElement('h1');
+
+
+
+//Appends
+myApp.appendChild(newH1);
+
+
+
+//Add text
+
+newH1.textContent = "Hellow World"
