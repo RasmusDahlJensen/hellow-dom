2e8e10481b36e4ddd8cdcc9a98aecd80d2b8fc48
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-18 12:31:46 +0200

    Reworked to function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,28 +2,30 @@
 const myApp = document.getElementById('app');
 
 
-//Create elements
+buildCard("This is a test", "/assets/placeholder.jpg", "This is description placeholder")
+buildCard("This is a placeholder", "/assets/placeholder.jpg", "This is description placeholder")
+buildCard("This is a placeholder", "/assets/placeholder.jpg", "This is description placeholder")
+
+function buildCard(cardHeadline, cardUrl, cardDescription) {
+    //Create elements
 const newH1 = document.createElement('h1');
 const firstFigure = document.createElement("figure")
 const galleryTitle = document.createElement("h2")
 const galleryImg = document.createElement("img")
-galleryImg.src = "/assets/placeholder.jpg"
+galleryImg.src = cardUrl
 const imgDescription = document.createElement("p")
 
+//Add text
+galleryTitle.textContent = cardHeadline
+imgDescription.textContent = cardDescription
+
+//Img style
+galleryImg.style.width = "20%"
 
 //Appends
 myApp.appendChild(newH1);
 myApp.appendChild(firstFigure)
 firstFigure.appendChild(galleryTitle)
 firstFigure.appendChild(galleryImg)
-firstFigure.appendChild(imgDescription)
-
-
-//Add text
-
-newH1.textContent = "Hellow World"
-galleryTitle.textContent = "Card title"
-imgDescription.textContent = "Picture of a city -- Placeholder description"
-
-//Img style
-galleryImg.style.width = "30%"
+firstFigure.appendChild(imgDescription)   
+}
