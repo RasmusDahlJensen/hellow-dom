bb2d1d9c8f91474d28ed53c809a1e170d723132b
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-18 09:45:30 +0200

    gallerycard aflevering

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,14 +4,26 @@ const myApp = document.getElementById('app');
 
 //Create elements
 const newH1 = document.createElement('h1');
-
+const firstFigure = document.createElement("figure")
+const galleryTitle = document.createElement("h2")
+const galleryImg = document.createElement("img")
+galleryImg.src = "/assets/placeholder.jpg"
+const imgDescription = document.createElement("p")
 
 
 //Appends
 myApp.appendChild(newH1);
-
+myApp.appendChild(firstFigure)
+firstFigure.appendChild(galleryTitle)
+firstFigure.appendChild(galleryImg)
+firstFigure.appendChild(imgDescription)
 
 
 //Add text
 
 newH1.textContent = "Hellow World"
+galleryTitle.textContent = "Card title"
+imgDescription.textContent = "Picture of a city -- Placeholder description"
+
+//Img style
+galleryImg.style.width = "30%"
